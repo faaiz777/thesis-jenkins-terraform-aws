90cc3449634d836be47b8b1b215e18279e6ef6a1
faaiz777 <faaiz0076@gmail.com>
2022-08-24 18:00:44 +0200

    Moularized

--- modules/remoteBackend/main.tf
+++ modules/remoteBackend/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket        = var.bucket_name # REPLACE WITH YOUR BUCKET NAME
-  #force_destroy = true
+  force_destroy = true
   
   lifecycle {
     prevent_destroy = false
