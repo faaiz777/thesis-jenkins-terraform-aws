8c8d25510874a79b05cb43237b83730b88a1f9bc
faaiz777 <faaiz0076@gmail.com>
2022-08-25 00:25:42 +0200

    Modulorized

--- main.tf
+++ main.tf
@@ -16,9 +16,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "eu-central-1"
-  #access_key = var.access_key
-  #secret_key = var.secret_key
-  #token = var.token
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,5 +1,3 @@
-#variable "access_key" {}
-#variable "secret_key" {}
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
